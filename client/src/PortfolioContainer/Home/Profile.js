729b6f3bad21b5f36ff9da64dcf8e7855b489d9b
Alyson Williams <alysonrwilliams@gmail.com>
2022-03-22 09:06:30 -0400

    some of the intro

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Typical from 'react-typical';
+import './Profile.css';
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="colz">
+                        <div className="colz-icon">
+                        <a href="https://www.facebook.com/aly.williams.351">
+                            <i className="fa fa-facebook-square"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/alyson-williams-mpa">
+                            <i className="fa fa-linkedin"></i>
+                        </a>
+                        <a href="https://www.instagram.com/howdy_aly/">
+                            <i className="fa fa-instagram"></i>
+                        </a>
+                        <a href="https://twitter.com/howdy_aly">
+                            <i className="fa fa-twitter"></i>
+                        </a>
+                        </div>
+                    </div>
+
+                <div className="profile-details-name">
+                    <span className="primary-text">
+                        {" "}
+                        Hello, I am <span className="highlighted-text">Alyson</span>
+                    </span>
+                </div>
+                <div className="profile-details-role">
+                    <span className="primary-text">
+                        {" "}
+                        <h1>
+                            {" "}
+                            <Typical
+                            loop={Infinity}
+                            steps={[
+                                "Problem Solver 💡", 
+                                1000,
+                                "HTML/CSS 💡", 
+                                1000,
+                                "React 💡", 
+                                1000,
+                                "Javascript 💡", 
+                                1000,
+                                "Front End Developer 💡", 
+                                1000
+                            ]}
+                            />
+                        </h1>
+                    
+                    <span className="profile-role-tagline">
+                    A fundamental developer with a forward thinking approach!
+                    </span>
+                    </span>
+                </div>
+                <div className="profile-options">
+                    <button className="btn primary-btn">
+                        {""}
+                        Hire Me{" "}
+                    </button>
+                    <a href=".ehizcv.pdf" download="Ehiedu ehizcv.pdf">
+                        <button className="btn highlighted-btn">Get Resume</button>
+                    </a>
+                </div>
+                </div>
+                <div className="profile-picture">
+                    <div className="profile-picture-background">
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
