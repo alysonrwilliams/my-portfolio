5a65f27ca18de533dfa61dd63a586a31bf9b628e
Alyson Williams <alysonrwilliams@gmail.com>
2022-03-28 13:33:40 -0400

    added about me section js and css files

---
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react';
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
+import "./AboutMe.css";
+
+export default function AboutMe(props) {
+    let fadeInScreenHandler = (screen)=> {
+        if(screen.fadeScreen !== props.id)
+        return
+        Animations.animations.fadeInScreen(props.id)
+    }
+const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+const SCREEN_CONSTANTS = {
+    description:
+      "Front end web and mobile developer with background knowledge of JavaScript and React, along with a knack for building and launching full websites with the utmost efficiency. Strong problem solver with a diverse education, ranging from code bootcamps to a traditional university masters program.",
+    highlights: {
+      bullets: [
+        "Front End Web Development",
+        "Interactive Front End as per the design",
+        "React and React Native",
+        "Full Lifecycle Web Maintenance",
+        "Utilizing REST API",
+        "Problem Solving with Teams and Independently",
+      ],
+      heading: "Here are a Few Highlights:",
+    },
+  };
+  const renderHighlight = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
+    return (
+        <div className="about-me-container screen-container" id={props.id || ""}>
+            <div className="about-me-parent">
+                <ScreenHeading title={'About Me'} subHeading ={'Why Choose Me?'} />
+                <div className="about-me-card">
+                    <div className="about-me-profile"></div>
+                    <div className="about-me-details">
+                        <span className="about-me-description">{SCREEN_CONSTANTS.description}</span>
+                        <div className="about-me-highlights">
+                            <div className="highlight-heading">
+                                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+                            </div>
+                            {renderHighlight()}
+                        </div>
+                        <div className="about-me-options">
+                        <button className="btn primary-btn">
+                        {""}
+                        Hire Me{" "}
+                    </button>
+                    <a href=".ehizcv.pdf" download="Ehiedu ehizcv.pdf">
+                        <button className="btn highlighted-btn">Get Resume</button>
+                    </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
