b9b6425c137a1a72b04ed1b4b9454b99befa7399
Alyson Williams <alysonrwilliams@gmail.com>
2022-03-29 12:46:06 -0400

    added more testimonoial section

--- client/src/PortfolioContainer/Testimonial/Testimonial.js
+++ client/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -2,6 +2,7 @@ import React from 'react';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+import "./Testimonial.css";
 
 import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
 import ScrollService from '../../utilities/ScrollService';
@@ -16,13 +17,35 @@ export default function Testimonial(props) {
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
+    const options ={
+        loop: true,
+        margin: 0,
+        nav: true,
+        animateIn: "bounceInRight",
+        animateOut: "bounceOutRight",
+        dots: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0:{
+                items: 1,
+            },
+            768:{
+                items: 1,
+            },
+            1000:{
+                items: 3,
+            },
+        }
+    }
+
   return (
     <div>
       <ScreenHeading title={'Testimonial'} subHeading={'Positive Praise!'} />
       <section className="testimonial-section" id={props.id || ''}>
         <div className="container">
           <div className="row">
-            <OwlCarousel className="owl-carousel" id="testimonial-carousel">
+            <OwlCarousel className="owl-carousel" id="testimonial-carousel" {...options}>
               
               <div className="col-lg-12">
                 <div className="testi-item">
@@ -165,11 +188,13 @@ export default function Testimonial(props) {
                   </div>
                 </div>
               </div>
-
             </OwlCarousel>
           </div>
         </div>
       </section>
+      <div className="footer-image">
+          <img src="../../assets/Testimonial/shape-bg.png" alt="shape" />
+      </div>
     </div>
   );
 }
